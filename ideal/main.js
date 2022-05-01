1ae75c1e85debc98d25e249ba3b1c1bbfc8f3679
SEOCHAEEUN <seochaeeun961126@gmail.com>
2022-05-01 18:25:33 +0900

    change

---
+++ ideal/main.js
@@ -0,0 +1,55 @@
+let candis = [
+  "Naver",
+  "Kakao",
+  "Line",
+  "Coupang",
+  "우아한 형제들",
+  "당근 마켓",
+  "TOSS",
+  "삼성전자",
+  "구글 코리아",
+  "퀄컴 코리아",
+  "SAP LABS KOREA",
+  "SSG.com",
+  "롯데쇼핑 E-commerce",
+  "11번가",
+  "위메프",
+  "티몬",
+  "직방",
+  "야놀자",
+  "크래프톤",
+  "NEXON",
+  "Netmarble",
+  "NHN",
+  "NC SOFT",
+  "Smilegate",
+  "농협 은행",
+  "신한 은행",
+  "국민 은행",
+  "SKT",
+  "KT",
+  "EST SOFT",
+  "한화 에어로 스페이스",
+  "Ahn Lab",
+  "SK shieldus",
+];
+
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+}
+
+let sorted_candis = shuffle(candis);
+let new_candis = [];
+
+console.log(sorted_candis);
+console.log(sorted_candis);
+
+function show() {
+  for (i = 0; i < 32; i++) {
+    new_candis.append();
+  }
+}
+
+first_place = document.querySelector("#first");
+second_place = document.querySelector("#second");
+for (let i = 0; i <= candis.length; i++) {}
