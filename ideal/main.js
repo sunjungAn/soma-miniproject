6ddb505948f49f83dd2e5d2ecaf18ebfad1c5ca0
sunjunaAn <dkstjswjd83@naver.com>
2022-05-01 19:39:01 +0900

    feat: update ideal pate

--- ideal/main.js
+++ ideal/main.js
@@ -1,4 +1,5 @@
-let candis = [
+
+const candis = [
   "Naver",
   "Kakao",
   "Line",
@@ -30,26 +31,69 @@ let candis = [
   "KT",
   "EST SOFT",
   "한화 에어로 스페이스",
-  "Ahn Lab",
-  "SK shieldus",
+  "Ahn Lab"
 ];
 
 function shuffle(array) {
   array.sort(() => Math.random() - 0.5);
 }
 
-let sorted_candis = shuffle(candis);
-let new_candis = [];
+//let sorted_candis = shuffle(candis);
+//let new_candis = [];
 
-console.log(sorted_candis);
-console.log(sorted_candis);
+//console.log(sorted_candis);
+//console.log(sorted_candis);
 
-function show() {
-  for (i = 0; i < 32; i++) {
-    new_candis.append();
+// function show() {
+//   for (i = 0; i < 32; i++) {
+//     new_candis.append();
+//   }
+// }
+
+const first = document.getElementById('first');
+const second = document.getElementById('second');
+
+// first_place = document.querySelector("#first");
+// second_place = document.querySelector("#second");
+
+let element1 ="";
+let element2 ="";
+
+function show(num){
+  if(num == 1){
+    candis.push(element1);
   }
+  else if(num == 2){
+    candis.push(element2);
+  }
+  
+  if(candis.length==16){
+    alert('16강!!! 진출!!!');
+  }
+  else if(candis.length == 8){
+    alert('8강!!! 진출!!!');
+  }
+  else if(candis.length == 4){
+    alert('준결승!!! 진출!!!');
+  }
+  else if(candis.length == 2){
+    alert('결승!!! 진출!!!');
+  }
+
+  first.textContent = candis[0];
+  if(candis.length == 1){
+    alert(candis[0]);
+    location.href = "";
+  }
+  else{
+    second.textContent = candis[1];
+  }
+  element1 = candis[0];
+  element2 = candis[1];
+  candis.shift();
+  candis.shift();
+  console.log(candis)
 }
 
-first_place = document.querySelector("#first");
-second_place = document.querySelector("#second");
-for (let i = 0; i <= candis.length; i++) {}
+show();
+
